06a43b0ce8947745acdd2c2bcd122406c07428a6
Rimantas <belauzas@gmail.com>
2023-06-19 14:22:17 +0300

    Clicker begins

--- js/main.js
+++ js/main.js
@@ -1,34 +1,10 @@
 console.clear();
 
-// Paspaudus <button> elementa
-// <h1> elemente pasikeicia tekstas
-
-// 1. Susirasti <button> elementa
-// 2. Jei <button> elementa YRA:
-// - susirandame <h1> elementa...
-//   - jei <h1> elementa YRA:
-//   - uzregistruojame ivykio stebejima:
-//      - kai/jei vartotojas paspaus <button>
-//      - <h1> elemente keiciam teksta
-//   - jei NERA:
-//   - nieko nedarom....
-// - jei NERA:
-// - nieko nedarom....
-
 const buttomDOM = document.getElementById('mygtukas');
+const titleDOM = document.getElementById('antraste');
 
-if (buttomDOM === null) {
-    console.log('Mygtuko nera :(');
-} else {
-    console.log('Mygtukas rastas!');
-
-    const titleDOM = document.getElementById('antraste');
-
-    if (titleDOM === null) {
-        console.log('Nepavyko rasti antrastes :(');
-    } else {
-        console.log('Antraste egzistuoja!');
-
-        titleDOM.textContent = 'Labas vakaras!';
-    }
+function kaDaryti() {
+    titleDOM.textContent = 'Labas vakaras!';
 }
+
+buttomDOM.addEventListener('click', kaDaryti);
