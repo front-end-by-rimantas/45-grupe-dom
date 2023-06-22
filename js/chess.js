bf5d2417ea467c8e4c381c956d1364c699b8c8c5
Rimantas <belauzas@gmail.com>
2023-06-22 13:53:22 +0300

    Chess - initial solution

--- js/chess.js
+++ js/chess.js
@@ -1,10 +1,48 @@
 import { renderHeader } from '../js/header.js';
-
 renderHeader();
 
+const formDOM = document.querySelector('form');
+const inputDOM = formDOM.querySelector('input');
+const boardDOM = document.querySelector('.board');
+
+formDOM.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const number = parseInt(inputDOM.value);
+
+    if (number > 0 && number % 1 === 0) {
+        renderChessBoard(number);
+    }
+});
+
 function renderChessBoard(size = 8) {
-    console.log(size);
+    let HTML = '';
+
+    for (let i = 0; i < size; i++) {
+        if (i % 2 === 0) {
+            HTML += '<div class="row">';
+            for (let j = 0; j < size; j++) {
+                if (j % 2 === 0) {
+                    HTML += '<div class="cell black"></div>';
+                } else {
+                    HTML += '<div class="cell"></div>';
+                }
+            }
+            HTML += '</div>';
+        } else {
+            HTML += '<div class="row">';
+            for (let j = 0; j < size; j++) {
+                if (j % 2 === 0) {
+                    HTML += '<div class="cell"></div>';
+                } else {
+                    HTML += '<div class="cell black"></div>';
+                }
+            }
+            HTML += '</div>';
+        }
+    }
+
+    boardDOM.innerHTML = HTML;
 }
 
-renderChessBoard(4);
 renderChessBoard();
